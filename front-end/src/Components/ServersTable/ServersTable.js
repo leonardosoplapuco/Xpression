0411683c07eaeea9f4954ab91bf6e73bca4c6842
leosoplapuco <leosoplapuco29@gmail.com>
2024-04-16 16:52:25 -0500

    Add: ServersTable and Footer folders

---
+++ front-end/src/Components/ServersTable/ServersTable.js
@@ -0,0 +1,11 @@
+import './ServersTable.css'
+
+function ServersTable() {
+    return(
+        <div className="block-container">
+            <div className="ServersTable"></div>
+        </div>
+    );
+}
+
+export default ServersTable;
