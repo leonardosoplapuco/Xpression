066f8dcb35848d95852bc2f64476b87d293b4b83
leosoplapuco <leosoplapuco29@gmail.com>
2024-04-16 14:35:49 -0500

    Add: front-end folder with a base model to the components header, hero and login

---
+++ front-end/src/Components/Login/Login.js
@@ -0,0 +1,24 @@
+import './Login.css'
+
+function Login() {
+    return (
+        <form className="login">
+            <div className="login-section">
+                <label htmlFor="Username" className="login-section_icon">
+                    <span class="material-symbols-outlined">alternate_email</span>
+                </label>
+                <input class="login-section_input" id="Username" placeholder="XMPP address"></input>
+            </div>
+            <div className="login-section">
+                <label htmlFor="Password" className="login-section_icon">
+                    <span class="material-symbols-outlined">lock</span>
+                </label>
+                <input class="login-section_input" id="Password" placeholder="Password"></input>
+            </div>
+            <button type="submit" class="submit" id="Login-submit">Sign In</button>
+            <a href="/" class="button-link login-signUp_link">Sign Up</a>
+        </form>
+    );
+}
+
+export default Login;
