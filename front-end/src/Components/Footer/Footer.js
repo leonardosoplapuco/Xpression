b22fff6de7fea5babfb92716fae58231c69d6ff4
Diego <diegonabe@hotmail.com>
2024-04-17 11:46:19 -0500

    Add. Footer

--- front-end/src/Components/Footer/Footer.js
+++ front-end/src/Components/Footer/Footer.js
@@ -3,8 +3,23 @@ import './Footer.css';
 
 function Footer() {
     return (
+<<<<<<< HEAD
+        <footer className="footer">
+            <nav className="footer-menu">
+                <ul>
+                    <li><div>Contacto</div></li>
+                    <li><div>Términos y Condiciones</div></li>
+                    <li><div>Política de Privacidad</div></li>
+                    <li><div>Mapa del Sitio</div></li>
+                </ul>
+            </nav>
+            <div className="footer-info">
+                <p>&copy; 2024 Xpression. Todos los derechos reservados.</p>
+            </div>
+        </footer>
+=======
         <p>Diego 123456</p>
-        
+>>>>>>> f024aa6bd925016bc94fa8ccd1c686880cbb2105
     );
 }
 
