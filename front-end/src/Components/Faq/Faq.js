b8a334c2b94e022a942314b15b4d603c43f97755
Kevin Huaynates <6992@holbertonstudents.com>
2024-04-25 02:36:05 -0500

    FAQ Uploaded

--- front-end/src/Components/Faq/Faq.js
+++ front-end/src/Components/Faq/Faq.js
@@ -8,27 +8,34 @@ function Faq() {
 
                 <div className="faq-target faq-target_1 active" onClick={faqOneActive}>
                     <div className="faq-target_title_content">
-                        <p className="faq-target_title">Question #1</p>
+                        <p className="faq-target_title">¿Xpression es seguro?</p>
                         <span className="material-symbols-outlined">expand_more</span>
                     </div>
-                    <p className="faq-target_content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s.</p>
+                    <p className="faq-target_content">Sí, Xpression es una aplicación segura que utiliza el protocolo XMPP (Extensible Messaging and Presence Protocol). El protocolo XMPP proporciona una capa de seguridad mediante el cifrado de extremo a extremo de los mensajes, lo que significa que los mensajes enviados entre los usuarios están protegidos y solo pueden ser leídos por el remitente y el destinatario.</p>
                 </div>
 
                 <div className="faq-target faq-target_2" onClick={faqTwoActive}>
                     <div className="faq-target_title_content">
-                        <p className="faq-target_title">Question #2</p>
+                        <p className="faq-target_title">¿Xpression recopila información sobre mis conversaciones o actividades en la aplicación?</p>
                         <span className="material-symbols-outlined">expand_more</span>
                     </div>
-                    <p className="faq-target_content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s.</p>
+                    <p className="faq-target_content">Xpression respeta tu privacidad y no recopila ni almacena información sobre tus conversaciones o actividades en la aplicación.</p>
                 </div>
 
                 <div className="faq-target faq-target_3" onClick={faqThreeActive}>
                     <div className="faq-target_title_content">
-                        <p className="faq-target_title">Question #3</p>
+                        <p className="faq-target_title">¿Puedo acceder a Xpression desde múltiples dispositivos?</p>
                         <span className="material-symbols-outlined">expand_more</span>
                     </div>
-                    <p className="faq-target_content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s.</p>
+                    <p className="faq-target_content">Sí, puedes acceder a Xpression desde múltiples dispositivos siempre y cuando inicies sesión con la misma cuenta en cada dispositivo. Tus conversaciones y datos estarán sincronizados entre dispositivos para una experiencia de usuario coherente.</p>
                 </div>
+
+                <div className="faq-target faq-target_4" onClick={faqFourActive}>
+                    <div className="faq-target_title_content">
+                        <p className="faq-target_title">¿Xpression ofrece funciones de llamadas de voz o video?</p>
+                        <span className="material-symbols-outlined">expand_more</span>
+                    </div>
+                    <p className="faq-target_content"> Actualmente, Xpression se enfoca en la mensajería instantánea y no ofrece funciones de llamadas de voz o video. Sin embargo, estamos constantemente evaluando las necesidades de nuestros usuarios y considerando nuevas características para futuras actualizaciones.</p>                                                                                                                     		    </div>
             </div>
         </div>
     );
