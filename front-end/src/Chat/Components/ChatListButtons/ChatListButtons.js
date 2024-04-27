17517b706ec729a7befdf5131da4ad8b987ab19d
leosoplapuco <leosoplapuco29@gmail.com>
2024-04-27 10:55:31 -0500

    Adding the AddContact component

--- front-end/src/Chat/Components/ChatListButtons/ChatListButtons.js
+++ front-end/src/Chat/Components/ChatListButtons/ChatListButtons.js
@@ -1,11 +1,12 @@
 import './ChatListButtons.css';
 import {ActiveLayerBlur} from '../LayerBlur/LayerBlur';
+import {ActiveAddContact} from '../AddContact/AddContact';
 import {ActiveSettings} from '../Settings/Settings';
 
 function ChatListButtons() {
     return(
         <ul className="ChatListButtons">
-            <li><button><span className="material-symbols-outlined button-link" title="Add contact">person_add</span></button></li>
+            <li><button onClick={ActiveAddContact}><span className="material-symbols-outlined button-link" title="Add contact" onClick={ActiveLayerBlur}>person_add</span></button></li>
             <li><button><span className="material-symbols-outlined button-link" title="Create a group">group_add</span></button></li>
             <li><button onClick={ActiveSettings}><span className="material-symbols-outlined button-link SettingsButton" title="Settings" onClick={ActiveLayerBlur}>tune</span></button></li>
         </ul>
